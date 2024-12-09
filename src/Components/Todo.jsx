6b9276515986d7ba9e6a8d983d19a3a3b840cf41
Dima <d.gotonoaga@outlook.com>
2024-12-09 16:06:25 +0200

    Add new Task function implemented

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 import { LuListTodo } from 'react-icons/lu';
 import { IoIosAddCircle } from 'react-icons/io';
@@ -6,6 +6,18 @@ import { MdDeleteOutline } from 'react-icons/md';
 import { TbPointFilled } from 'react-icons/tb';
 
 const Todo = () => {
+  const [task, setTask] = useState('');
+  const [list, setList] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() != '') {
+      setList([...list, task]);
+      setTask('');
+    }
+
+    console.log([...list]);
+  };
+
   return (
     <div className="main">
       <div className="main-header">
@@ -17,14 +29,21 @@ const Todo = () => {
           className="todo-input"
           type="text"
           placeholder="Add a New Task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
         />
-        <IoIosAddCircle className="add-icon" />
+        <IoIosAddCircle className="add-icon" onClick={addTask} />
       </div>
-
       <div className="lists">
-        <input type="checkbox" name="" id="" />
-        <input type="text" readOnly />
-        <MdDeleteOutline className="delete-icon" />
+        {list.map((task, index) => {
+          return (
+            <div key={index} className="Alllists">
+              <input type="checkbox" name="" id="" />
+              <input type="text" readOnly value={task} />
+              <MdDeleteOutline className="delete-icon" />
+            </div>
+          );
+        })}
       </div>
       <hr />
       <div className="summary">
